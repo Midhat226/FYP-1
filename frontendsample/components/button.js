17322e2406a6fe8bf947daaa26a73e538ba40de4
tatheer18 <93919359+tatheer18@users.noreply.github.com>
2023-01-03 19:42:32 +0500

    Update button.js

--- frontendsample/components/button.js
+++ frontendsample/components/button.js
@@ -1,22 +1,23 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableWithoutFeedback } from 'react-native';
 
 const GradientButton = ({ text,onPress }) => (
-  <TouchableOpacity onPress={onPress}>
+  <TouchableWithoutFeedback onPress={onPress}>
   <LinearGradient colors={['#a2d4cf', '#7bece1', '#11d1be']} 
       style={styles.linearGradient} 
       start={{ y: 0.0, x: 0.0 }} end={{ y: 0.0, x: 1.0 }}>
         <Text style={styles.text}>{text}</Text>
   </LinearGradient>
-  </TouchableOpacity>
+  </TouchableWithoutFeedback>
 );
 
 const styles = StyleSheet.create({
  linearGradient: {
     flex: 1,
-    paddingLeft: 15,
+    paddingLeft: 10,
     paddingRight: 15,
-    marginTop: 10,
+    marginTop: 15,
     borderRadius: 15,
   },
   text: {
